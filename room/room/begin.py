489917345245ce4af3a36c02a53553fb4c70e853
氵便玩玩 <130010534+2only1@users.noreply.github.com>
2023-09-20 23:56:29 +0800

    Update begin.py

--- room/room/begin.py
+++ room/room/begin.py
@@ -1,4 +1,2 @@
 from scrapy.cmdline import execute
-# execute(['scrapy', 'crawl', 'lianjia'])
-# execute(['scrapy', 'crawl', 'lianjia2'])
-execute(['scrapy', 'crawl', 'lianjia3'])
+execute(['scrapy', 'crawl', 'lianjia'])
